2459f5215e09d0b67c8733eb13bb78c689f236a3
LeelaBrhmani <leela999.ad@gmail.com>
2024-04-01 16:36:53 +0530

    springboot

---
+++ src/main/java/com/cyr/resrful/RestfullExampleApplication.java
@@ -0,0 +1,13 @@
+package com.cyr.resrful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfullExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfullExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cyr/resrful/RestfullExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyr.resrful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfullExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
